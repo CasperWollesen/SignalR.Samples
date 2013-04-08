1746c659817561a4634ba84d8a39dab3a91bc7a2
Casper Wollesen <CasperWollesen@GMail.com>
2013-04-08 13:42:43 +0200

    Added WPF Demo

---
+++ BasicChat.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
+namespace BasicChat.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public System.Threading.Thread Thread { get; set; }
+        public string Host = "http://localhost:8081/";   // "http://localhost:8081/" | "http://localhost:44914/"
+
+        public IHubProxy ProxyInvoke { get; set; }
+        public HubConnection ConnectionInvoke { get; set; }
+
+        public bool Active { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ActionSendButtonClick(object sender, RoutedEventArgs e)
+        {
+            await SendMessage();
+        }
+
+        private async Task SendMessage()
+        {
+            await ProxyInvoke.Invoke("send", ClientNameTextBox.Text + ": " + MessageTextBox.Text);
+        }
+
+        private async void ActionWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Active = true;
+            Thread = new System.Threading.Thread(() =>
+            {
+                var connectionOn = new HubConnection(Host);
+                IHubProxy chatOn = connectionOn.CreateHubProxy("Chat");
+
+                chatOn.On<string>("send", OnSendData);
+
+                connectionOn.Start();
+
+                while (Active)
+                {
+                    System.Threading.Thread.Sleep(10);
+                }
+            }) { IsBackground = true };
+            Thread.Start();
+
+            ConnectionInvoke = new HubConnection(Host);
+            ProxyInvoke = ConnectionInvoke.CreateHubProxy("Chat");
+            await ConnectionInvoke.Start();
+        }
+
+        private void OnSendData(string message)
+        {
+            Dispatcher.Invoke(DispatcherPriority.Normal, (Action)(() => MessagesListBox.Items.Insert(0, message))); 
+        }
+
+        private async void ActionMessageTextBoxOnKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter || e.Key == Key.Return)
+            {
+                await SendMessage();
+                MessageTextBox.Text = "";
+            }
+        }
+    }
+}
--- BasicChatConsole/Program.cs
+++ BasicChatConsole/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR.Client.Hubs;
 
@@ -16,23 +13,43 @@ namespace BasicChatConsole
 
         private static async Task RunAsync()
         {
-            Console.WriteLine("Press anter to start client: ");
-            Console.ReadLine();
-            
-            var connection = new HubConnection("http://localhost:8081/");
+            var active = true;
+
+            var host = "http://localhost:8081/"; // "http://localhost:44914/";
+
+            Console.WriteLine("Please enter client name: ");
+            var clientName = Console.ReadLine();
+
+            var thread = new System.Threading.Thread(() =>
+                                                         {
+                                                             var connectionOn = new HubConnection(host);
+                                                             IHubProxy chatOn = connectionOn.CreateHubProxy("Chat");
+
+                                                             chatOn.On<string>("send", s => Console.WriteLine("Console: " + s));
+
+                                                             connectionOn.Start();
+
+                                                             while (active)
+                                                             {
+                                                                 System.Threading.Thread.Sleep(10);
+                                                             }
+                                                         }) { IsBackground = true };
+            thread.Start();
+
+            var connection = new HubConnection(host);
             IHubProxy chat = connection.CreateHubProxy("Chat");
-
-            chat.On<string>("send", s => Console.WriteLine("Console: " + s));
-
             await connection.Start();
 
-            Console.WriteLine("Enter new line: ");
+            Console.Write("Enter new line: ");
             string line = null;
             while ((line = Console.ReadLine()) != null)
             {
-                await chat.Invoke("send", line);
+                await chat.Invoke("send", clientName + ": " + line);
                 Console.Write("Enter new line: ");
             }
+
+            active = false;
+            thread.Join();
         }
     }
 }
